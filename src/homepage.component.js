e6823f25970fdb3562aa4f53c6c76bebfc4ac35b
rui-watanabe <wataru610806@gmail.com>
2020-06-10 20:42:38 +0900

    change homepage style

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,45 +1,38 @@
 import React from 'react';
+import './homepage.style.scss';
 
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
       <div className="menu-item">
-       <div className="content">
-         <h1 className="title">HATS</h1>
-         <span className="title">SHOW NOW</span>
-       </div>
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOW NOW</span>
+        </div>
       </div>
-    </div>
-    <div className="directory-menu">
       <div className="menu-item">
-       <div className="content">
-         <h1 className="title">JACKETS</h1>
-         <span className="title">SHOW NOW</span>
-       </div>
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="title">SHOW NOW</span>
+        </div>
       </div>
-    </div>
-    <div className="directory-menu">
       <div className="menu-item">
-       <div className="content">
-         <h1 className="title">SNEAKERS</h1>
-         <span className="title">SHOW NOW</span>
-       </div>
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="title">SHOW NOW</span>
+        </div>
       </div>
-    </div>
-    <div className="directory-menu">
       <div className="menu-item">
-       <div className="content">
-         <h1 className="title">WOMENS</h1>
-         <span className="title">SHOW NOW</span>
-       </div>
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="title">SHOW NOW</span>
+        </div>
       </div>
-    </div>
-    <div className="directory-menu">
       <div className="menu-item">
-       <div className="content">
-         <h1 className="title">MENS</h1>
-         <span className="title">SHOW NOW</span>
-       </div>
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="title">SHOW NOW</span>
+        </div>
       </div>
     </div>
   </div>
